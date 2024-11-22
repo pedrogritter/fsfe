dd6f2dc71710e198c6ffb0f86f6a3b4c0b98d9a0
Pedro Gritter <pedro12345g@gmail.com>
2024-11-22 01:51:33 +0000

    add db connection

--- index-ws.js
+++ index-ws.js
@@ -16,6 +16,19 @@ const webSocketServer = require("ws").Server;
 
 const wss = new webSocketServer({ server: server });
 
+process.on("SIGINT", () => {
+  console.log("sigint");
+
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  console.log("sigint db");
+
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 wss.on("connection", function connection(ws) {
   const numClients = wss.clients.size;
   console.log("Clients connected: ", numClients);
@@ -25,6 +38,10 @@ wss.on("connection", function connection(ws) {
   if (ws.readyState === ws.OPEN) {
     ws.send("Welcom to my server");
   }
+
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))
+    `);
 
   ws.on("close", function close() {
     wss.broadcast("A client has disconnected");
@@ -37,3 +54,31 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+// End websockets
+// Start DB
+
+const sql = require("sqlite3");
+
+const db = new sql.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  console.log("Shutting down DB");
+  getCounts();
+  db.close();
+}
